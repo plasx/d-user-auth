1b9a64e64a72aee68a451672b5c6517246adb2be
Daniel Rivera <plasx@live.com>
2019-12-17 21:22:57 -0500

    created user login verification and modified template to show login status

--- mysite/authenticate/views.py
+++ mysite/authenticate/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def home(request):
     return render(request, 'authenticate/home.html', {})
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ('You have logged In!'))
+            return redirect('home')
+        else:
+            messages.success(request, ('Error Logging in - Please Try Again'))
+            return redirect('login')
+    else:
+        return render(request, 'authenticate/login.html', {})
