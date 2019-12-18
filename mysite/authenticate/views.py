21ec7a816c48a3c006495edc5455fb9efd476805
Daniel Rivera <plasx@live.com>
2019-12-18 11:50:05 -0500

    created registration

--- mysite/authenticate/views.py
+++ mysite/authenticate/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     return render(request, 'authenticate/home.html', {})
-
 
 def login_user(request):
     if request.method == 'POST':
@@ -21,3 +20,26 @@ def login_user(request):
             return redirect('login')
     else:
         return render(request, 'authenticate/login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ('You have been logged out!'))
+    return redirect('home')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            messages.success(request, ('You have successully registered'))
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+
+    context = {'form': form}
+    return render(request, 'authenticate/register.html', context)
