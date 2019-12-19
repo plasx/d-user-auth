26f2039b47a61ed9cf7e350892c5af88095ec820
Daniel Rivera <plasx@live.com>
2019-12-18 20:55:55 -0500

    added change password view and form and template

--- mysite/authenticate/forms.py
+++ mysite/authenticate/forms.py
@@ -1,6 +1,13 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django import forms
 from django.contrib.auth.models import User
+
+
+class EditProfileForm(UserChangeForm):
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email',)
 
 
 class SignUpForm(UserCreationForm):
--- mysite/authenticate/views.py
+++ mysite/authenticate/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .forms import SignUpForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from .forms import SignUpForm, EditProfileForm
 
 def home(request):
     return render(request, 'authenticate/home.html', {})
@@ -48,13 +48,28 @@ def register_user(request):
 
 def edit_profile(request):
     if request.method == 'POST':
-        form = UserChangeForm(request.POST, instance=request.user)
+        form = EditProfileForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
             messages.success(request, ('You have edited your profile.'))
             return redirect('home')
     else:
-        form = UserChangeForm(instance=request.user)
+        form = EditProfileForm(instance=request.user)
 
     context = {'form': form}
     return render(request, 'authenticate/edit_profile.html', context)
+
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(data=request.POST, user=request.user)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            messages.success(request, ('You have edited your profile.'))
+            return redirect('home')
+    else:
+        form = PasswordChangeForm(user=request.user)
+
+    context = {'form': form}
+    return render(request, 'authenticate/change_password.html', context)
